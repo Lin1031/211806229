66236558379f8a6fac05bb02d91fb92fa11cc03c
Lin1031 <44975556+Lin1031@users.noreply.github.com>
2019-11-18 17:34:32 +0800

    Add files via upload

---
+++ 2homework.cpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+struct Data
+{
+	char senderName[10];
+	char receiverName[10];
+	int data;
+}sender[20000];
+struct Hacker
+{
+	char hackerName[10]; 
+}hacker[100];
+int main()
+{
+	FILE *read, *write;
+	read = fopen("test.txt", "r");
+	if(read == NULL)
+	{
+		printf("ERROR");
+		return -1;
+	} 
+	int num=0;
+	while(!feof(read))
+	{
+		fscanf(read,"%s %s %d",&sender[num].senderName,&sender[num].receiverName,&sender[num].data);
+		num++;
+	}
+	fclose(read);
+	int i,j,count=0;
+	for(i=0;i<num;i++)
+	{
+		if(sender[i].data>=100)
+		{
+			for(j=0;j<count;j++)
+			{
+				strcpy(hacker[j].hackerName,sender[i].senderName);
+				count++;
+			} 
+		}
+	}
+	printf("%d\n",count);
+	for(i=0;i<count;i++)
+	{
+		printf("%s\n",hacker[i].hackerName);
+	}
+	return 0;
+ } 
